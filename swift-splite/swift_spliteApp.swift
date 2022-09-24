dba83c57806b59bcca7c6ce81400cf6af8039ca9
JustinLien0805 <6998dragoan@gmail.com>
2022-09-24 16:55:12 +0800

    Initial Commit

---
+++ swift-splite/swift_spliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_spliteApp.swift
+//  swift-splite
+//
+//  Created by 練家辰 on 2022/9/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_spliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
